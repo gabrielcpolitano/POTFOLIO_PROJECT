d1e4dd8ff7e301d641a9c89411e0dcddf5a9de62
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2024-10-30 18:30:45 -0300

    Thanks God - PDF

--- Fundamentos/Inglês/2- Fonética/B.js
+++
@@ -1,9 +0,0 @@
-const b = () => {
-  console.log('Book: búk')
-  console.log('Bicycle: báy-sih-kuhl')
-  console.log('Beach: bítch')
-  console.log('Butterfly: bút-er-flai')
-  console.log('Balloon: bə-lún')
-}
-
-b()

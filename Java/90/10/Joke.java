5561f4cc281a4c7b17ca6fc9529572b4f32c56ac
gabrielcpolitano <gabriel.cpolitano@gmail.com>
2025-04-22 16:06:47 -0300

    Updates exercises

---
+++ Java/90/10/Joke.java
@@ -0,0 +1,6 @@
+public class Joke {
+  public static void main(String[] args) {
+    System.out.println("Why did the programmer quit his job?");
+    System.out.println("Because he didn't get arrays!");
+  }
+}
